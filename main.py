c578d1f7ffcdd5873987bc6e624a2e667902fb75
monteship <monteship@gmail.com>
2025-04-27 08:58:51 +0300

    refactor main.py to use config.py for environment variable management and improve code clarity

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-import os
 from loguru import logger
 
 import time
 import threading
 
-from dotenv import load_dotenv
-
+from config import MQTT_USER, MQTT_PASSWORD, MQTT_BROKER, MQTT_PORT, NETWORK
 from device import search_devices
 from mqtt_handler import handle_set_params, handle_get_params
 import paho.mqtt.client as mqtt
 
-# Load environment variables
-load_dotenv()
-NETWORK = os.getenv("NETWORK", "localhost").split(",")
-MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
-MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
-MQTT_USER = os.getenv("MQTT_USER", "")
-MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "")
-MQTT_TOPIC = os.getenv("MQTT_TOPIC", "gree")
-UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", 4))
 
 # Configure loguru
 logger.add("logs/app.log", rotation="1 MB", retention="7 days", level="INFO")
@@ -36,13 +25,13 @@ mqtt_client.loop_start()
 if __name__ == "__main__":
     threads = []
     for device_ip in NETWORK:
-        device = search_devices(device_ip)
-        if device:
-            logger.info(f"Device found: {device}")
+        d = search_devices(device_ip)
+        if d:
+            logger.info(f"Device found: {d}")
             # Start a thread for periodic updates
             thread = threading.Thread(
                 target=handle_get_params,
-                args=(device, mqtt_client, MQTT_TOPIC, UPDATE_INTERVAL),
+                args=(d, mqtt_client),
             )
             thread.daemon = True
             threads.append(thread)
@@ -51,7 +40,7 @@ if __name__ == "__main__":
             # Start a thread to handle set_params
             set_thread = threading.Thread(
                 target=handle_set_params,
-                args=(device, mqtt_client, MQTT_TOPIC),
+                args=(d, mqtt_client),
             )
             set_thread.daemon = True
             threads.append(set_thread)
