02bb4ea03e37e8dae0d001bf817fb56475dfad23
monteship <monteship@gmail.com>
2025-05-03 01:09:13 +0300

    refactor: update config.py to use type hints for improved clarity and type safety

--- config.py
+++ config.py
@@ -1,25 +1,26 @@
 import os
 
+from typing import Optional, List
 from dotenv import load_dotenv
 
 
 # Load environment variables
 load_dotenv()
-NETWORK = os.getenv("NETWORK", "localhost").split(",")
+NETWORK: List[str] = os.getenv("NETWORK", "192.168.1.40,192.168.1.41").split(",")
 
 # Set MQTT parameters
-MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_BROKER: str = os.getenv("MQTT_BROKER", "192.168.1.10")
 MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
-MQTT_USER = os.getenv("MQTT_USER", "")
-MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "")
-MQTT_TOPIC = os.getenv("MQTT_TOPIC", "gree")
+MQTT_USER: Optional[str] = os.getenv("MQTT_USER")
+MQTT_PASSWORD: Optional[str] = os.getenv("MQTT_PASSWORD")
+MQTT_TOPIC: str = os.getenv("MQTT_TOPIC", "gree")
 
 # Set update interval
-UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", 4))
+UPDATE_INTERVAL: int = int(os.getenv("UPDATE_INTERVAL", 4))
 
 # Set default tracking parameters
 # Pow,Mod,SetTem,TemUn,WdSpd,Air,Blo,Health,SwhSlp,Lig,SwingLfRig,SwUpDn,Quiet,Tur,StHt,HeatCoolType,TemRec,SvSt,TemSen
-TRACKING_PARAMS = os.getenv(
+TRACKING_PARAMS: List[str] = os.getenv(
     "TRACKING_PARAMS",
     "Pow,Mod,SetTem,WdSpd,SwhSlp,Lig,SwUpDn,Quiet,Tur,StHt,TemSen",
 ).split(",")
