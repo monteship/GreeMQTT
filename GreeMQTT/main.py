d2cb2a17e8920cf59e86e806947ee31e39b1c892
monteship <monteship@gmail.com>
2025-05-04 17:03:01 +0300

    refactor: streamline device handling in main function and improve missing device search logic

--- GreeMQTT/main.py
+++ GreeMQTT/main.py
@@ -1,66 +1,52 @@
 import asyncio
 
+from typing import List
+
 from GreeMQTT import logger
 from GreeMQTT.config import NETWORK
 from GreeMQTT.mqtt_client import create_mqtt_client
 from GreeMQTT.device import Device
 from GreeMQTT.device_db import device_db
 from GreeMQTT.managers import DeviceRetryManager, start_device_tasks
+from GreeMQTT.mqtt_handler import handle_set_params
 
 
 async def main():
-    tasks = []
     stop_event = asyncio.Event()
 
     # Load known devices from DB
-    known_devices = {
-        mac: (ip, key, is_gcm) for mac, ip, key, is_gcm in device_db.get_all_devices()
-    }
-    missing_devices = []
+    known_devices: List[Device] = device_db.get_all_devices()
+    known_ips = [device.device_ip for device in known_devices]
+    missing_devices = [ip for ip in NETWORK if ip not in known_ips]
 
     async with await create_mqtt_client() as mqtt_client:
-        for device_ip in NETWORK:
-            device = None
-            # Try to use known device from DB
-            for mac, (ip, key, is_gcm) in known_devices.items():
-                if ip == device_ip:
-                    device = Device(
-                        device_ip=ip,
-                        device_id=mac,
-                        name="Load from DB",
-                        is_GCM=is_gcm == 1,
-                        key=key,
-                    )
-                    logger.info(f"Loaded device from DB: {device}")
-                    break
-            # If not found in DB, search for the device
-            if not device:
-                device = await Device.search_devices(device_ip)
-                if device and device.key:
-                    logger.info(f"Device found: {device}")
-                    device_db.save_device(
-                        device.device_id, device.ip, device.key, device.is_GCM
-                    )
-                else:
-                    logger.warning(f"Device not found: {device_ip}")
-                    missing_devices.append(device_ip)
-            if device:
-                # Start async tasks for periodic updates
-                tasks.extend(await start_device_tasks(device, mqtt_client, stop_event))
+        # Start tasks for known devices
+        for device in known_devices:
+            await start_device_tasks(device, mqtt_client, stop_event)
 
+        # If not found in DB, search for the device
+        for device_ip in missing_devices:
+            device = await Device.search_devices(device_ip)
+            if device and device.key:
+                logger.info(f"Device found: {device}")
+                device_db.save_device(
+                    device.device_id, device.device_ip, device.key, device.is_GCM
+                )
+                missing_devices.remove(device_ip)
+                await start_device_tasks(device, mqtt_client, stop_event)
         # Start retry task
         if missing_devices:
             retry_manager = DeviceRetryManager(missing_devices, mqtt_client, stop_event)
-            retry_task = asyncio.create_task(retry_manager.run())
-            await retry_task
+            asyncio.create_task(retry_manager.run())
         else:
             logger.info("All devices found.")
+
+        await asyncio.create_task(handle_set_params(mqtt_client, stop_event))
 
         # Keep the main task alive
         try:
             while True:
-                await asyncio.sleep(1)
+                await asyncio.sleep(0.1)
         except KeyboardInterrupt:
             logger.info("Exiting...")
             stop_event.set()
-            await asyncio.gather(*tasks, return_exceptions=True)
